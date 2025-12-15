b7c33c3d019358423ef6e06cac7d48aea27973d2
Kayan Hendges <kayanbhendges@gmail.com>
2025-12-15 11:50:13 -0300

    Configure GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/mavi-growth-hub">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/sobre" element={<Sobre />} />
